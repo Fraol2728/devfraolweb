c5b4820e1660369cc7ae6387d5fb2806d7a95fa3
Dev Fraol <158087611+Fraol2728@users.noreply.github.com>
2026-02-23 14:45:33 +0300

    Update InstructorBio.jsx

--- src/features/instructor/InstructorBio.jsx
+++ src/features/instructor/InstructorBio.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import profileImage from "@/assets/profile.png";
+import profileImage from "@/assets/devfraol.jpg";
 import { SocialLinks } from "@/features/instructor/SocialLinks";
 
 const bioPoints = [
