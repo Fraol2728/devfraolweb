d7e11991f7e78ea64fdf2adc14001677dc076a6b
Dev Fraol <158087611+Fraol2728@users.noreply.github.com>
2026-02-26 21:14:47 -0800

    Delete server directory

--- server/routes/courseRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getCourseBySlug, getCourses } from "../controllers/courseController.js";
-
-const router = Router();
-
-router.get("/", getCourses);
-router.get("/:slug", getCourseBySlug);
-
-export default router;
